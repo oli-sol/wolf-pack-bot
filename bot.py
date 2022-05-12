7837f122cf8cbebd6b272a7cd11001dc57ceac88
Nader <nedh84@users.noreply.github.com>
2022-05-11 23:16:24 -0700

    Set up a discord 'cog' for handling various events. We can create additional cogs for various functionalities but for now we have the Zephyr cog

--- bot.py
+++ bot.py
@@ -1,7 +1,20 @@
 # bot.py
+import os
+import discord
+from discord.ext import commands
+from dotenv import load_dotenv
 
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
 
-print("Hello World, my name is Saber")
-print("Okay have fun nerds")
-print("My turn")
-print("branch 4")
+initial_extensions = ["zephyr"]
+
+bot = commands.Bot(command_prefix='!', description="bot stuff")
+
+# Here we load our extensions(cogs) listed above in [initial_extensions].
+if __name__ == '__main__':
+    for extension in initial_extensions:
+        bot.load_extension(extension)
+
+bot.run(TOKEN)
