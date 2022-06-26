247077887638312506c7f963689d0a4ccadb9544
osolorzano3 <osolorzano3@gatech.edu>
2022-06-26 14:40:20 -0700

    Added command to pull BiS data from DB

--- cogs/raid.py
+++ cogs/raid.py
@@ -1,3 +1,4 @@
+import discord
 from discord.ext import commands
 from google.cloud import datastore
 
@@ -11,10 +12,51 @@ class Raid(commands.Cog):
         self.db_client = datastore.Client()
 
     @commands.command()
-    async def bis(self, ctx):
-        query = self.db_client.query(kind="Member")
-        results = list(query.fetch())
-        await ctx.send(results)
+    async def bis(self, ctx, role: discord.Role, cmd, item="None"):
+        if cmd.lower() == "needed":
+            # fetch all entities from DB for a specific role name/kind/table
+            query = self.db_client.query(kind=role.name)
+            results = list(query.fetch())
+
+            # create dict to store response data
+            needed = {}
+
+            # loop through each member, add item to list if missing
+            for member in results:
+                needed[member["Name"]] = ""
+
+                if not member["Weapon"]:
+                    needed[member["Name"]] += "Weapon, "
+                if not member["Head"]:
+                    needed[member["Name"]] += "Head, "
+                if not member["Chest"]:
+                    needed[member["Name"]] += "Chest, "
+                if not member["Hands"]:
+                    needed[member["Name"]] += "Hands, "
+                if not member["Legs"]:
+                    needed[member["Name"]] += "Legs, "
+                if not member["Feet"]:
+                    needed[member["Name"]] += "Feet, "
+                if not member["Earring"]:
+                    needed[member["Name"]] += "Earring, "
+                if not member["Necklace"]:
+                    needed[member["Name"]] += "Necklace, "
+                if not member["Bracelet"]:
+                    needed[member["Name"]] += "Bracelet, "
+                if not member["Ring"]:
+                    needed[member["Name"]] += "Ring, "
+
+                # cleanup string to remove comma and space at end
+                needed[member["Name"]] = needed[member["Name"]][:-2]
+
+            # if dict isn't empty, send
+            if needed:
+                await ctx.send("The following items are still needed for BiS! *wags tail*\n\n")
+
+                # construct response string, send
+                for key, value in needed.items():
+                    response = key + ": " + value
+                    await ctx.send(response)
 
 
 def setup(client):
